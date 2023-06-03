7b5598c0b52d596ff360ad4f80a3beff063755e6
Merza Bolivar <merza.bolivar@gmail.com>
2023-06-03 19:31:05 +0700

    feat: add drawer menu

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/providers/category_provider.dart';
+import 'package:todo_app/providers/task_provider.dart';
 import 'package:todo_app/screens/add_task/add_task.dart';
 import 'package:todo_app/screens/calendar/calendar.dart';
 import 'package:todo_app/screens/task_list/task_list.dart';
 import 'package:todo_app/widgets/menu_button.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   int index = 0;
 
   final List<Widget> _pages = [
@@ -36,6 +39,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final tasks = ref.watch(tasksProvider);
+    final categories = ref.watch(categoriesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo App'),
@@ -47,6 +53,108 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+              accountName: const Text('Merza Bolivar'),
+              accountEmail: const Text('Merza.bolivar@Gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                child: Text(
+                  'A',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.workspace_premium_outlined,
+                color: ColorScheme.fromSeed(
+                  seedColor: Colors.yellow,
+                  brightness: Theme.of(context).brightness,
+                ).primary,
+              ),
+              title: Text(
+                'Become a Pro Member',
+                style: TextStyle(
+                  color: ColorScheme.fromSeed(
+                    seedColor: Colors.yellow,
+                    brightness: Theme.of(context).brightness,
+                  ).primary,
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(Icons.star),
+              title: const Text('Starred Tasks'),
+            ),
+            ExpansionTile(
+              initiallyExpanded: true,
+              shape: ShapeBorder.lerp(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                1,
+              ),
+              leading: const Icon(Icons.category),
+              title: const Text('Categories'),
+              children: [
+                ...categories.map(
+                  (category) => Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: ListTile(
+                      onTap: () {},
+                      leading: Icon(category.icon),
+                      title: Text(category.name),
+                      trailing: Text(
+                        tasks.where((element) => element.category == category).length.toString(),
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: ListTile(
+                    onTap: () {},
+                    leading: const Icon(Icons.add),
+                    title: const Text('Add Category'),
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(Icons.favorite),
+              title: const Text('Donate'),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(Icons.question_mark),
+              title: const Text('FAQ'),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+            ),
+          ],
+        ),
       ),
       body: _pages[index],
       floatingActionButton: index == 0
